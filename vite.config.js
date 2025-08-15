43f5658cfe5c018ae65b42aad2c4275a87ec159b
Reagan Sajulga <rgnsajulga@gmail.com>
2025-08-15 13:31:00 +0800

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project-1/' // 👈 repo name
   // define:{
   //   'process.env.VITE_KEY': JSON.stringify(process.env.VITE_KEY)
   // }
